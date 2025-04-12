a6017b8baa698318f0f80fbf5ca43f6b1d3249bd
owen-chw <c040466bo@gmail.com>
2025-04-12 12:50:34 +0800

    add addition

---
+++ app.py
@@ -0,0 +1,19 @@
+
+class calculator:
+    def __init__(self):
+        pass
+
+    def addition(self, a, b):
+        return a+b
+    
+    def run(self):
+        while True:
+            a = int(input("This is a binary calculator, please input the first number: "))
+            b = int(input("Then, input the second number: "))
+            ret = self.addition(a, b)
+            print("addition: "+str(ret))
+
+if __name__ == "__main__":
+    cal = calculator()
+    cal.run()
+
